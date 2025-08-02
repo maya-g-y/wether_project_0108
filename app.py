c4483572fc072f80f396f0441ba1f4ede29b0dd0
maya-g-y <maya.geva77@gmail.com>
2025-08-02 19:08:29 +0300

    Added 'search history' & 'location map'

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
+from datetime import datetime
 from src.data import fetch_weather
 from src.ui import show_weather
 
 st.set_page_config(page_title="Weather Checker", page_icon="🌦️")
 
-st.title('Weather App')
+st.title('🌦️Weather App')
 
 API_KEY = "5961cc2151e8c770260cde5abdd694aa"
+
+if "search_history" not in st.session_state:
+    st.session_state.search_history = []
+
 with st.form(key="weather_form"):
     city = st.text_input("Enter the city name")
     submit = st.form_submit_button("Get Weather")
@@ -14,4 +19,29 @@ with st.form(key="weather_form"):
 if submit and city:
     response = fetch_weather(city, API_KEY)
     data = response.json()
-    show_weather(data, city)
+
+    if data["cod"] == 200:
+        show_weather(data, city)
+
+        st.session_state.search_history.append({
+            "city": city,
+            "temperature": data['main']['temp'],
+            "description": data['weather'][0]['description'].capitalize(),
+            "time": datetime.now().strftime("%H:%M")
+        })
+    else:
+        st.error("City not found or API error.")
+
+st.sidebar.title("🕘 Search History")
+
+if st.sidebar.button("🗑️ Clear History"):
+    st.session_state.search_history.clear()
+
+if st.session_state.search_history:
+    for item in reversed(st.session_state.search_history):
+        st.sidebar.markdown(
+            f"**{item['city']}** ({item['time']})  \n"
+            f"{item['temperature']}°C – {item['description']}"
+        )
+else:
+    st.sidebar.info("No searches yet.")
--- src/ui.py
+++ src/ui.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import folium
+from streamlit_folium import folium_static
 
 def show_weather(data, city):
     if data["cod"] == 200:
@@ -8,6 +10,9 @@ def show_weather(data, city):
         icon_code = data['weather'][0]['icon']
         icon_url = f"http://openweathermap.org/img/wn/{icon_code}@2x.png"
 
+        lat = data['coord']['lat']
+        lon = data['coord']['lon']
+
         st.subheader(f"Weather in {city}")
         st.image(icon_url, width=100)
         st.markdown("### ☁️ Weather Summary")
@@ -16,6 +21,11 @@ def show_weather(data, city):
         col1.metric("🌡️ Temperature (°C)", f"{temperature}°")
         col2.metric("💧 Humidity (%)", f"{humidity}%")
         col3.metric("☁️ Conditions", description.capitalize())
+
+        st.markdown('### 🗺️Location Map')
+        m = folium.Map(location=[lat, lon], zoom_start=12)
+        folium.Marker([lat, lon], popup=city).add_to(m)
+        folium_static(m)
 
     else:
         st.error("City not found or API error. Please check the city name.")
